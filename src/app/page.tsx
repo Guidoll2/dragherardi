44a72d47d6fbb6d44bd3d46f4abc3387d8282f3e
Guido Llaurado <guido.llaurado@gmail.com>
2025-04-30 20:03:30 -0300

    English first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   const [hover, setHover] = useState(false);
-  const [language, setLanguage] = useState("ES");
+  const [language, setLanguage] = useState("EN");
 
   // Animation variants
   const stagger = {
