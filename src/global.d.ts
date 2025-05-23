77c373cff3d6b33fe5a19b66ed22ac180bc0ffe8
Guidoll2 <guido.llaurado@gmail.com>
2025-05-23 18:08:29 -0300

    gdbt

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,7 +3,7 @@ import { Mongoose } from 'mongoose';
 declare global {
   // Se ha cambiado 'var' a 'let' para cumplir con las mejores prácticas de TypeScript/ESLint.
   // 'let' es apropiado aquí porque 'mongoose.conn' y 'mongoose.promise' pueden ser reasignados.
-  let mongoose: {
+  var mongoose: {
     conn: Mongoose | null;
     promise: Promise<Mongoose> | null;
   } | undefined;
