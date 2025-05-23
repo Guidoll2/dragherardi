bf359573987aa2b0beeef476929dd4bab0356a54
Guidoll2 <guido.llaurado@gmail.com>
2025-05-23 18:17:09 -0300

    global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,16 @@
-import { Mongoose } from 'mongoose';
+import { Mongoose } from "mongoose";
 
 declare global {
-  // Se ha cambiado 'var' a 'let' para cumplir con las mejores prácticas de TypeScript/ESLint.
-  // 'let' es apropiado aquí porque 'mongoose.conn' y 'mongoose.promise' pueden ser reasignados.
+  namespace NodeJS {
+    interface Global {
+      mongoose: {
+        conn: Mongoose | null;
+        promise: Promise<Mongoose> | null;
+      };
+    }
+  }
+
+  // Alternativa moderna: extender globalThis directamente
   var mongoose: {
     conn: Mongoose | null;
     promise: Promise<Mongoose> | null;
